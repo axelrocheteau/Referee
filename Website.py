f787c56cb43eaa09fa5b873cf4e17db3364efd95
axel rocheteau <axel.rocheteau4@gmail.com>
2024-10-13 14:14:00 +0200

    winamax

--- Website.py
+++ Website.py
@@ -1,15 +1,45 @@
 from dataclasses import dataclass
+from Bet import Bet
 
 @dataclass
 class Website:
     name: str
     url : str
     page_content : str
-    organised_page_content : list[dict[str, str|list[float]|list[str]]]
+    organised_page_content : dict[str : list[dict[str : str|list[float]|list[str]]]]
     bets : list[Bet]
 
     def __init__(self, name, url):
         self.name = name
         self.url = url
+        self.page_content = ''
+        self.organised_page_content = {}
+        self.bets = []
     
-    def get_page_content()
+    def get_page_content(self):
+        pass
+
+    def get_sport_content(self, sport):
+        pass
+    
+    def organise_content(self):
+        pass
+
+    def organise_bets(self):
+        for sport, matches in self.organised_page_content.items():
+            for match in matches:
+                self.bets.append(Bet(
+                    match['team1'],
+                    match['team2'],
+                    sport,
+                    self.name,
+                    match['odds'],
+                    match['odds_descriptions']
+                    )
+                )
+    
+    def place_bet(self, team1, team2, sport, odd, odd_description):
+        pass
+
+    def login(self):
+        pass
--- Websites/Winamax.py
+++ Websites/Winamax.py
@@ -1,8 +1,65 @@
 from dataclasses import dataclass
+import requests
+import json
+import re
 from Website import Website
+from Bet import Bet
 
 @dataclass
 class Winamax(Website):
 
+    def __init__(self, name, url):
+        super(Winamax, self).__init__(name, url)
+
     def get_page_content(self):
-        pass
+        response = requests.get(self.url, headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"})
+        html = response.text
+        extract_json_re = re.compile('(?<=var PRELOADED_STATE = )(.*)(?=;</script><script type="text/javascript">)')
+        json = re.search(extract_json_re, html).group(0)
+        self.page_content = json
+
+    def organise_content(self):
+        json_formatted = json.loads(self.page_content)
+
+        sports = json_formatted["sports"]
+        matches = json_formatted["matches"]
+        bets = json_formatted["bets"]
+        outcomes = json_formatted["outcomes"]
+        odds = json_formatted["odds"]
+
+        for betid, bet in bets.items():
+            ## exclude nonetype bets
+            if bet is None:
+                continue
+
+            ## exclude alternative bets
+            if bet["isAlternativeMainBet"] == "true":
+                continue
+            
+            # Teams name
+            match_id = bet["matchId"]
+            title = matches[str(match_id)]["title"]
+            team1, team2 = title.split(" - ")
+
+            #sport name
+            sport_id = matches[str(match_id)]["sportId"]
+            sport_name = sports[str(sport_id)]["sportName"]
+
+            # odds
+            bet_odds = []
+            bet_descriptions = []
+            for outcome_id in bet["outcomes"]:
+                bet_odds.append(odds[str(outcome_id)])
+                bet_descriptions.append(outcomes[str(outcome_id)]["label"])
+
+            # sport already created
+            if sport_name in self.organised_page_content:
+                self.organised_page_content[sport_name].append({"team1" : team1, "team2" : team2, "odds" : bet_odds, "odds_descriptions" : bet_descriptions})
+            # sport not created
+            else:
+                self.organised_page_content[sport_name] = [{"team1" : team1, "team2" : team2, "odds" : bet_odds, "odds_descriptions" : bet_descriptions}]
+
+    def organise_bets(self):
+        super().organise_bets()
+
+    
